ba9e02c073501c012a503c3d79c96a0a43bcd29f
Spandan Dhru <sdhru29@gmail.com>
2025-08-14 21:47:34 -0400

    cleaned comments, preparing for updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
 const { Player } = require("discord-player")
 const eventHandler = require('./handlers/eventHandler');
+const { YoutubeiExtractor, createYoutubeiStream } = require("discord-player-youtubei");
+const { SpotifyExtractor } = require('@discord-player/extractor');
+const { DefaultExtractors } = require('@discord-player/extractor');
 
 const client = new Client({
   intents: [
@@ -15,11 +18,11 @@ const client = new Client({
 
 eventHandler(client);
 
-client.player = new Player(client, {
-  ytdlOptions: {
-      quality: "highestaudio",
-      highWaterMark: 1 << 25
-  }
-})
+client.player = new Player(client);
+
+client.player.extractors.unregisterAll();
+
+client.player.extractors.register(YoutubeiExtractor, {});
+client.player.extractors.register(SpotifyExtractor, {});
 
 client.login(process.env.TOKEN);
